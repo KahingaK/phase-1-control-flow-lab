11db470aea5bb54a2a0fd204256db9790e4822c3
IvanKahinga <ikahinga@gmail.com>
2022-11-23 18:32:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+ let result;
+   if (someNumber <= 400){ result = "This one is on me!"
+      } else if (someNumber > 2000  && someNumber  <= 2499){result = "I will gladly take your thirty bucks."
+
+      } else {result = "No can do."}
+
+ return result;
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+
+
+
+
+function ternaryCheckCity(someCity){
+  let result;
+  someCity == "NYC" ? result = "Ok, sounds good." : result = "No go.";
+
+  return result;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmount){
+
+  switch (tipAmount) {
+    case 'generous':
+      return 'Thank you so much.'
+      
+      break;
+    
+    case 'not as generous':
+        return 'Thank you.'
+       
+        break
+
+    default: return "Bye."
+      break;
+  }
+  
 }
